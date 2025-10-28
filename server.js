5c31024761ff62f7d7e24d949779b03e756bf511
Daelin Campleman <127960675+Daelin-Campleman@users.noreply.github.com>
2025-10-28 09:49:21 +0200

    fixed field name

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post('/ticket', async (req, res) => {
                     description,
                     "customfield_10223": email,
                     "customfield_10224": phone,
-                    "organization": organization
+                    "organizations": organization
                 },
                 "raiseOnBehalfOf": email
             })
